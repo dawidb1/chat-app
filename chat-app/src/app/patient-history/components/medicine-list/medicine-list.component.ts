2a89bd7c3556f5d3cfbb738463ff3f6f48edeca9
Dawid Baranski <dawid.baranski.96@gmail.com>
2019-05-21 10:11:58 +0200

    login & submit forms

--- chat-app/src/app/patient-history/components/medicine-list/medicine-list.component.ts
+++ chat-app/src/app/patient-history/components/medicine-list/medicine-list.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit, Input, SimpleChanges, OnChanges} from '@angular/core';
-import { AppPage } from 'e2e/src/app.po';
-import { MEDICINES } from './mock-medicine';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { Medicine } from 'src/app/model/medicine-list.model';
 import { ClinicUserService } from 'src/app/authorization/services/clinic-user.service';
 import { ClinicUser } from 'src/app/authorization/model/clinic-user.model';
 import { UserType } from 'src/app/authorization/model/user-type.enum';
-import { MatDatepickerInputEvent, MatDialog } from '@angular/material';
-import { Subscriber } from 'rxjs';
+import { MatDialog } from '@angular/material';
 import { MedicineDialogComponent } from '../medicine-dialog/medicine-dialog.component';
-
 
 @Component({
   selector: 'app-medicine-list',
@@ -16,12 +12,14 @@ import { MedicineDialogComponent } from '../medicine-dialog/medicine-dialog.comp
   styleUrls: ['./medicine-list.component.scss']
 })
 export class MedicineListComponent implements OnInit, OnChanges {
+  constructor(private clinicUserService: ClinicUserService, public dialog: MatDialog) {}
   @Input() roomUserEmail: string;
   @Input() userType: UserType;
-  @Input() currentUserEmail:string;
+  @Input() currentUserEmail: string;
   med: Medicine[];
-  constructor(
-    private clinicUserService: ClinicUserService, public dialog: MatDialog) {}
+
+  // med=MEDICINES;
+  selectedMedicine: Medicine;
 
   ngOnInit() {
     this.getMedicineList();
@@ -30,44 +28,32 @@ export class MedicineListComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     this.getMedicineList();
   }
-   
-  //med=MEDICINES;
-  selectedMedicine: Medicine;
 
-  
   onSelect(medicine: Medicine): void {
     this.selectedMedicine = medicine;
 
-    let dialogRef = this.dialog.open(MedicineDialogComponent, {
+    const dialogRef = this.dialog.open(MedicineDialogComponent, {
       height: '400px',
-      width: '600px',
+      width: '600px'
     });
   }
-  
-  getMedicineList()
-  {
+
+  getMedicineList() {
     console.log('Get medicine list called');
     console.log(this.roomUserEmail);
-    
-    if(this.userType==UserType.PATIENT)
-    {
-        this.clinicUserService.getUserByEmail(this.currentUserEmail).subscribe((res: ClinicUser) => {
+
+    if (this.userType == UserType.PATIENT) {
+      this.clinicUserService.getUserByEmail(this.currentUserEmail).subscribe((res: ClinicUser) => {
         const clinicUser: ClinicUser = res;
-        this.med=res.Medicine_list;
-        return (res.Medicine_list);            
+        this.med = res.Medicine_list;
+        return res.Medicine_list;
       });
-    }
-    else if(this.userType==UserType.DOCTOR){
+    } else if (this.userType == UserType.DOCTOR) {
       this.clinicUserService.getUserByEmail(this.roomUserEmail).subscribe((res: ClinicUser) => {
         const clinicUser: ClinicUser = res;
-        this.med=res.Medicine_list;
-        return (res.Medicine_list);      
+        this.med = res.Medicine_list;
+        return res.Medicine_list;
       });
     }
   }
-
 }
-
-
-
-
