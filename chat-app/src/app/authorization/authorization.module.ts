34a436598e3346135a207b77e87403657bbecc00
matecebr <mateceb384@student.polsl.pl>
2019-05-21 22:57:06 +0200

    Revert "login & signup modal"
    This reverts commit 51750e35833c3b3d830c3809067934e91f228a6a.

--- chat-app/src/app/authorization/authorization.module.ts
+++ chat-app/src/app/authorization/authorization.module.ts
@@ -4,12 +4,9 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
 import { SharedModule } from '../shared/modules/shared.module';
 import { ImportModule } from '../shared/modules/import.module';
-import { LoginModuleComponent } from './components/login-module/login-module.component';
-import { LoginModalComponent } from './components/login-modal/login-modal.component';
-import { SignupModalComponent } from './components/signup-form/signup-modal/signup-modal.component';
 
 @NgModule({
-  declarations: [LoginFormComponent, SignupFormComponent, LoginModuleComponent, LoginModalComponent, SignupModalComponent],
+  declarations: [LoginFormComponent, SignupFormComponent],
   imports: [CommonModule, ImportModule, SharedModule]
 })
 export class AuthorizationModule {}
